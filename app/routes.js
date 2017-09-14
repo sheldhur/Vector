8e1b92447e892c985e05ccb84df086e7181ee19e
Sheldhur Mornor <drfreez@ya.ru>
2017-09-15 04:15:45 +0800

    nothing...

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { Route, IndexRoute, browserHistory} from 'react-router';
+import {Route, IndexRoute, browserHistory} from 'react-router';
 import App from './containers/App';
 import Home from './components/Home';
 import Main from './components/main/Main';
@@ -12,14 +12,14 @@ import Capture from './components/capture/Capture';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={Home} />
-    <Route path="/home" component={Home} />
-    <Route path="/main" component={Main} />
-    <Route path="/dataSet" component={DataSetView} />
-    <Route path="/dataSet/:id" component={DataSetValuesView} />
-    <Route path="/station" component={StationsView} />
-    <Route path="/station/:id" component={StationValuesView} />
-    <Route path="/capture" component={Capture} />
+    <IndexRoute component={Home}/>
+    <Route path="/home" component={Home}/>
+    <Route path="/main" component={Main}/>
+    <Route path="/dataSet" component={DataSetView}/>
+    <Route path="/dataSet/:id" component={DataSetValuesView}/>
+    <Route path="/station" component={StationsView}/>
+    <Route path="/station/:id" component={StationValuesView}/>
+    <Route path="/capture" component={Capture}/>
   </Route>
 );
 
