a0b974160ff2ba6a10697d4b491bb0f2f150832b
Sheldhur Mornor <drfreez@ya.ru>
2017-12-15 23:28:40 +0800

    Delete not usages

--- app/constants/dataSetImport.js
+++ app/constants/dataSetImport.js
@@ -1,6 +1,4 @@
 export const SHOW_MODAL = 'dataSetImport/SHOW_MODAL';
 export const PROGRESS_BAR = 'dataSetImport/PROGRESS_BAR';
-export const PROGRESS_BAR_INCREMENT = 'dataSetImport/PROGRESS_BAR_INCREMENT';
-export const PROGRESS_BAR_FILE = 'dataSetImport/PROGRESS_BAR_FILE';
 export const CURRENT_FILE = 'dataSetImport/CURRENT_FILE';
 export const IMPORT_LOG = 'dataSetImport/IMPORT_LOG';
