5468e7175433691222287df80eefac60b2562d93
Sheldhur Mornor <drfreez@ya.ru>
2017-12-28 00:55:04 +0800

    Fix prod store config

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,16 +1,41 @@
 // @flow
-import {createStore, applyMiddleware} from 'redux';
+import {electronEnhancer} from "redux-electron-store";
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import {createBrowserHistory} from 'history';
 import {routerMiddleware} from 'react-router-redux';
 import rootReducer from '../reducers';
 
 const history = createBrowserHistory();
-const router = routerMiddleware(history);
-const enhancer = applyMiddleware(thunk, router);
 
 function configureStore(initialState) {
-  return createStore(rootReducer, initialState, enhancer);
+  // Redux Configuration
+  const middleware = [];
+  const enhancers = [];
+
+  // Thunk Middleware
+  middleware.push(thunk);
+
+  // Router Middleware
+  const router = routerMiddleware(history);
+  middleware.push(router);
+
+  // Apply Middleware & Compose Enhancers
+  enhancers.push(applyMiddleware(...middleware));
+  enhancers.push(electronEnhancer({
+    actionFilter: (action) => {
+      if (action.syncState === true) {
+        return true;
+      }
+      return false;
+    }
+  }));
+  const enhancer = compose(...enhancers);
+
+  // Create Store
+  const store = createStore(rootReducer, initialState, enhancer);
+
+  return store
 }
 
 export default {configureStore, history};
